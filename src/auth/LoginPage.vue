2815ee4af709d4430374296407f00c48b04e269e
Bewizi <toluwalase.bewaji@yahoo.com>
2025-06-10 19:27:26 +0100

    add login and register page for users info and also give them permission

---
+++ src/auth/LoginPage.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+// import { useAuth } from '@/composables/useAuth'
+import httpClient from '@/libs/httpClients'
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { toast, type ToastOptions } from 'vue3-toastify'
+
+// const { login } = useAuth()
+
+const router = useRouter()
+const form = ref({
+  email: '',
+  password: '',
+})
+
+const handleSubmit = async () => {
+  try {
+    const response = await httpClient.post('/user/login', form.value)
+    response.data = form.value
+    console.log('Login Successful', response.data)
+
+    console.log('user data', form.value)
+    // await login(form.value)
+    toast.success('Login Successful', { autoclose: 3000 } as ToastOptions)
+    // alert('Login successful:')
+
+    router.push('/')
+  } catch (error) {
+    console.log(error)
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>Login Page</h1>
+    <p>Welcome to login page</p>
+
+    <form @submit.prevent="handleSubmit">
+      <label for="" class="block">Email</label>
+      <input type="email" placeholder="Enter email address" v-model="form.email" />
+
+      <label for="" class="block">Password</label>
+      <input type="text" placeholder="Enter password" v-model="form.password" />
+
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/auth/RegisterPage.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useAuth } from '@/composables/useAuth'
+import type { SignUpForm } from '@/types'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const form = ref<SignUpForm>({
+  email: '',
+  password: '',
+  firstName: '',
+  lastName: '',
+  phone: '',
+  role: 'homeowner', // Default role
+})
+
+const { register } = useAuth()
+const error = ref<string | null>(null)
+
+const onSubmit = async () => {
+  try {
+    await register(form.value)
+    // Redirect after signup (handled in router)
+    router.push('/')
+  } catch (err) {
+    console.log(error.value)
+    console.log('Error fetching data:', err)
+  }
+}
+</script>
+
+<template>
+  <form @submit.prevent="onSubmit">
+    <input v-model="form.email" type="email" placeholder="Email" required />
+    <input v-model="form.password" type="password" placeholder="Password" required />
+    <div>
+      <input v-model="form.firstName" placeholder="First Name" required />
+      <input v-model="form.lastName" placeholder="Last Name" required />
+    </div>
+    <input v-model="form.phone" type="tel" placeholder="Phone (+123...)" required />
+
+    <fieldset>
+      <legend>Role</legend>
+      <label>
+        <input v-model="form.role" type="radio" value="contractor" />
+        Contractor
+      </label>
+      <label>
+        <input v-model="form.role" type="radio" value="supplier" />
+        Supplier
+      </label>
+      <label>
+        <input v-model="form.role" type="radio" value="homeowner" />
+        Homeowner
+      </label>
+    </fieldset>
+
+    <button type="submit">Sign Up</button>
+    <p v-if="error" class="error">{{ error }}</p>
+  </form>
+</template>
