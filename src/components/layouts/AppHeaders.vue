dec86b6be3eb94f4a87ce17c28df854100922eae
Bewizi <toluwalase.bewaji@yahoo.com>
2025-06-10 19:39:49 +0100

    add AppHeader component to display navbar and logo

---
+++ src/components/layouts/AppHeaders.vue
@@ -0,0 +1,142 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import AppLink from '../AppLink.vue'
+import Button_UI from '../ui/Button_UI.vue'
+import { Icon } from '@iconify/vue'
+
+const showProducts = ref<boolean | null>(false)
+const showNavBarMobile = ref<boolean | null>(false)
+const toogleProducts = () => {
+  showProducts.value = !showProducts.value
+}
+
+const toogleOpenNavbar = () => {
+  showNavBarMobile.value = true
+}
+
+const toogleCLoseNavbar = () => {
+  showNavBarMobile.value = false
+}
+</script>
+
+<template>
+  <section class="mx-auto px-5 lg:px-8 py-10">
+    <header>
+      <!-- desktop -->
+      <div class="flex justify-between items-center">
+        <!-- logo -->
+        <div class="w-[80px] h-[80px] bg-slate-100 rounded-full">
+          <img
+            src="/images/Logo.png"
+            alt="Block Forge Logo"
+            class="block object-cover max-w-full"
+          />
+        </div>
+
+        <!-- nav -->
+        <nav class="hidden lg:flex lg:space-x-8 font-roboto_mono text-secondary_text">
+          <AppLink to="/">Home</AppLink>
+          <AppLink @click="toogleProducts" to="/">
+            <span class="flex items-center gap-2 relative"
+              >Produts
+              <Icon
+                :icon="showProducts ? 'iconamoon:arrow-up-2' : 'iconamoon:arrow-down-2'"
+                width="24"
+                height="24"
+                style="color: #000000"
+            /></span>
+
+            <!-- sublinks under products -->
+            <Transition>
+              <div v-show="showProducts" class="absolute top-24 p-5 bg-gray-200 w-[500px]">
+                <p>Hollow</p>
+                <p>Solid</p>
+              </div>
+            </Transition>
+          </AppLink>
+
+          <!-- about us -->
+          <AppLink to="/">About Us</AppLink>
+          <AppLink to="/">Projects</AppLink>
+          <AppLink to="/">Contractor Hub</AppLink>
+          <AppLink to="/">Contact</AppLink>
+        </nav>
+
+        <!-- login and signup button -->
+        <div class="md:flex gap-5 hidden">
+          <!-- login -->
+          <AppLink to="/auth/login">
+            <Button_UI>Login</Button_UI>
+          </AppLink>
+
+          <!-- register -->
+          <AppLink to="/auth/register ">
+            <Button_UI variant="secondary">Register</Button_UI>
+          </AppLink>
+        </div>
+
+        <!-- icons -->
+        <div class="flex lg:hidden">
+          <Icon
+            v-if="showNavBarMobile"
+            @click="toogleCLoseNavbar"
+            icon="ic:round-close"
+            width="24"
+            height="24"
+            style="color: #000000"
+          />
+
+          <Icon
+            v-else
+            @click="toogleOpenNavbar"
+            icon="fe:bar"
+            width="24"
+            height="24"
+            style="color: #000000"
+          />
+        </div>
+      </div>
+
+      <!-- mobile view nav -->
+      <nav v-show="showNavBarMobile" class="flex flex-col lg:hidden space-y-8">
+        <AppLink to="/">Home</AppLink>
+        <AppLink @click="toogleProducts" to="/">
+          <span class="flex items-center gap-2 relative"
+            >Produts
+            <Icon
+              :icon="showProducts ? 'iconamoon:arrow-up-2' : 'iconamoon:arrow-down-2'"
+              width="24"
+              height="24"
+              style="color: #000000"
+          /></span>
+
+          <!-- sublinks under products -->
+          <Transition>
+            <div v-show="showProducts" class="absolute top-50 p-5 bg-gray-200 w-[500px]">
+              <p>Hollow</p>
+              <p>Solid</p>
+            </div>
+          </Transition>
+        </AppLink>
+
+        <!-- about us -->
+        <AppLink to="/">About Us</AppLink>
+        <AppLink to="/">Projects</AppLink>
+        <AppLink to="/">Contractor Hub</AppLink>
+        <AppLink to="/">Contact</AppLink>
+      </nav>
+    </header>
+  </section>
+</template>
+
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
