178d8885ad9280a96500277b3907efa2560da5d5
Bewizi <toluwalase.bewaji@yahoo.com>
2025-06-10 19:42:06 +0100

    add router navigation between view

---
+++ src/router/index.ts
@@ -0,0 +1,62 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import HomeView from '../views/HomeView.vue'
+
+import PageLayout from '@/components/layouts/PageLayout.vue'
+import AuthLayout from '@/auth/AuthLayout.vue'
+
+const routes = [
+  {
+    path: '/',
+    component: PageLayout,
+    children: [
+      {
+        path: '',
+        name: 'home',
+        component: HomeView,
+      },
+      {
+        path: '/about',
+        name: 'about',
+        // route level code-splitting
+        // this generates a separate chunk (About.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import('../views/AboutView.vue'),
+      },
+    ],
+  },
+
+  {
+    path: '/auth',
+    component: AuthLayout,
+    children: [
+      {
+        path: 'login',
+        name: 'login',
+        component: () => import('@/auth/LoginPage.vue'),
+      },
+      {
+        path: 'register',
+        name: 'register',
+        component: () => import('@/auth/RegisterPage.vue'),
+      },
+    ],
+  },
+
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'notFound',
+    component: () => import('@/views/NotFoundView.vue'),
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+
+  scrollBehavior() {
+    return { top: 0 }
+  },
+})
+
+export default router
