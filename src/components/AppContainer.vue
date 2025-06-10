fd34e56d95b8dfc573671f565dcba2b017e48f15
Bewizi <toluwalase.bewaji@yahoo.com>
2025-06-10 19:28:28 +0100

    add appconatiner

---
+++ src/components/AppContainer.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  className?: string
+}>()
+</script>
+
+<template>
+  <section :class="'max-w-7xl mx-auto px-5 lg:px-8'">
+    <slot></slot>
+  </section>
+</template>
+
+<style scoped></style>
