23e5f2ef22d0cd253e5e2c199cc3c43d085c1350
Bewizi <toluwalase.bewaji@yahoo.com>
2025-06-10 19:29:11 +0100

    add AppLink rather than use router-link created a component to do that

---
+++ src/components/AppLink.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  to: string
+}>()
+</script>
+
+<template>
+  <RouterLink :to="to">
+    <slot></slot>
+  </RouterLink>
+</template>
+
+<style scoped></style>
