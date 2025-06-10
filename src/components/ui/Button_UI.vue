dff5d28ca6bc743bb27dcdfad97c4c61b4aa8f46
Bewizi <toluwalase.bewaji@yahoo.com>
2025-06-10 19:41:15 +0100

    add reuseable components Button and Card

---
+++ src/components/ui/Button_UI.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps<{
+  variant?: 'primary' | 'secondary'
+}>()
+
+const buttonClass = {
+  primary: 'bg-[#e67e22] py-2 px-8 text-white',
+  secondary: 'border-2 border-[#e67e22] py-2 px-8 text-[#2c3e50]',
+}
+</script>
+
+<template>
+  <button :class="buttonClass[variant || 'primary']">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped></style>
