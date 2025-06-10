53d8d7970323cdee9f577a3d75b1f0f9fb7cd51e
Bewizi <toluwalase.bewaji@yahoo.com>
2025-06-10 19:40:29 +0100

    add PageLayout

---
+++ src/components/layouts/PageLayout.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import AppFooter from './AppFooter.vue'
+import AppHeaders from './AppHeaders.vue'
+</script>
+
+<template>
+  <!-- PageLayout -->
+  <AppHeaders />
+  <RouterView />
+  <AppFooter />
+</template>
+
+<style scoped></style>
