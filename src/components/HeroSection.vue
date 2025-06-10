9c5a58e43a7c911f46fb843e3c661ef25c6b366d
Bewizi <toluwalase.bewaji@yahoo.com>
2025-06-10 19:29:48 +0100

    add HeroSection for the home view

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main
+    class="bg-[url('/images/Road_Construction.jpg')] h-screen bg-cover bg-no-repeat grid place-items-center"
+  >
+    <!-- hero text -->
+    <div class="text-center p-4 max-w-4xl mx-auto">
+      <h1 class="text-4xl md:text-6xl font-bold text-primary-heading mb-4 leading-tight font-saira">
+        Construct with Confidence
+      </h1>
+      <p
+        class="text-lg md:text-xl text-secondary-text mb-8 max-w-2xl mx-auto font-roboto_mono text-center"
+      >
+        Our robust concrete blocks are designed for lasting strength and exceptional results in any
+        construction endeavor.
+      </p>
+
+      <!-- button -->
+      <div class="flex gap-5 items-center justify-center">
+        <button
+          class="bg-accent_btn text-white px-8 py-3 rounded-full shadow-lg hover:bg-opacity-90 transition-all duration-300 transform hover:scale-105 font-roboto_mono"
+        >
+          Explore Our Products
+        </button>
+        <button
+          class="bg-white text-primary-bg border border-primary-heading px-8 py-3 rounded-full shadow-lg hover:bg-opacity-90 transition-all duration-300 transform hover:scale-105 font-roboto_mono"
+        >
+          Contact Us
+        </button>
+      </div>
+    </div>
+  </main>
+</template>
