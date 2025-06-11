3c27d1a15583d6748add4cef6ef1f42f9afa7844
Bewizi <toluwalase.bewaji@yahoo.com>
2025-06-11 21:22:01 +0100

    created a component to manage all the blocks

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import AppLink from './AppLink.vue'
+
+defineProps<{
+  hollow: {
+    id: string
+    name: string
+    image: string
+    category: string
+  }
+}>()
+</script>
+
+<template>
+  <div>
+    <AppLink :to="hollow.id">
+      <div>
+        <img :src="hollow.image" :alt="hollow.name" />
+        <h1>{{ hollow.name }}</h1>
+        <span>{{ hollow.category }}</span>
+      </div>
+    </AppLink>
+  </div>
+</template>
+
+<style scoped></style>
