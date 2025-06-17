41fe62464af0ab7a105715835ad46c416a57811e
Bewizi <toluwalase.bewaji@yahoo.com>
2025-06-17 15:03:25 +0100

    add styles and refactored the text

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,10 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AppContainer from '@/components/AppContainer.vue'
+import AppLink from '@/components/AppLink.vue'
+</script>
 
 <template>
-  <div>
-    <h1>Not Found Page</h1>
-    <p>Welcome to Not Found page</p>
-  </div>
+  <AppContainer class="py-16">
+    <div class="grid place-items-center">
+      <h1 class="text-primary_heading text-9xl font-saira font-extrabold mb-5">404</h1>
+      <p class="text-secondary_text font-roboto_mono mb-5 text-2xl">Not Found Page</p>
+      <AppLink to="/" class="hover:text-blue-500 text-secondary_text font-roboto_mono"
+        >Go Home</AppLink
+      >
+    </div>
+  </AppContainer>
 </template>
 
 <style scoped></style>
