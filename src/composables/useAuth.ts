cce54da438f655e0e6d7e40007b2c6ecf883ba9c
Bewizi <toluwalase.bewaji@yahoo.com>
2025-06-19 14:03:16 +0100

    committed out all codes

--- src/composables/useAuth.ts
+++ src/composables/useAuth.ts
@@ -1,56 +1,56 @@
-import { supabase } from '@/libs/supabaseClients'
-import type { LoginForm, SignUpForm } from '@/types'
+// import { supabase } from '@/libs/supabaseClients'
+// import type { LoginForm, SignUpForm } from '@/types'
 
-export const useAuth = () => {
-  const register = async (form: SignUpForm) => {
-    const { data: authUser, error: authError } = await supabase.auth.signUp({
-      email: form.email,
-      password: form.password,
-    })
+// export const useAuth = () => {
+//   const register = async (form: SignUpForm) => {
+//     const { data: authUser, error: authError } = await supabase.auth.signUp({
+//       email: form.email,
+//       password: form.password,
+//     })
 
-    if (authError) throw authError
+//     if (authError) throw authError
 
-    console.log('Auth user:', authUser.user?.id)
+//     console.log('Auth user:', authUser.user?.id)
 
-    // if (!authError) {
-    //   throw new Error('User creation failed')
-    // }
+//     // if (!authError) {
+//     //   throw new Error('User creation failed')
+//     // }
 
-    const { error: profileError } = await supabase.from('profile').insert({
-      // id: authUser.user?.id,
-      user_id: authUser.user?.id,
-      firstName: form.firstName,
-      lastName: form.lastName,
-      phone: form.phone,
-      role: form.role,
-    })
+//     const { error: profileError } = await supabase.from('profile').insert({
+//       // id: authUser.user?.id,
+//       user_id: authUser.user?.id,
+//       firstName: form.firstName,
+//       lastName: form.lastName,
+//       phone: form.phone,
+//       role: form.role,
+//     })
 
-    console.log('Profile insert result:', { data: authUser, profileError })
+//     console.log('Profile insert result:', { data: authUser, profileError })
 
-    if (profileError) throw profileError
-  }
+//     if (profileError) throw profileError
+//   }
 
-  const login = async (form: LoginForm) => {
-    const { data: authUser, error: authError } = await supabase.auth.signInWithPassword({
-      email: form.email,
-      password: form.password,
-    })
+//   const login = async (form: LoginForm) => {
+//     const { data: authUser, error: authError } = await supabase.auth.signInWithPassword({
+//       email: form.email,
+//       password: form.password,
+//     })
 
-    if (authError) throw authError
+//     if (authError) throw authError
 
-    console.log('Login successful:', authUser.user?.id)
-    // alert('Login successful:')
-    return authUser
-  }
+//     console.log('Login successful:', authUser.user?.id)
+//     // alert('Login successful:')
+//     return authUser
+//   }
 
-  const logout = async () => {
-    const { error } = await supabase.auth.signOut()
-    if (error) throw error
-  }
+//   const logout = async () => {
+//     const { error } = await supabase.auth.signOut()
+//     if (error) throw error
+//   }
 
-  return {
-    register,
-    login,
-    logout,
-  }
-}
+//   return {
+//     register,
+//     login,
+//     logout,
+//   }
+// }
